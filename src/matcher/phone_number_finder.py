c37c140e69c631c65bcf2bd520a2f880ba7c304b
James Rocker <james.rocker@simplybusiness.co.uk>
2022-05-26 11:44:35 +0100

    clean up comments

--- src/matcher/phone_number_finder.py
+++ src/matcher/phone_number_finder.py
@@ -1,8 +1,6 @@
 """
 The problem with search for phone numbers is that it produces false positives.
 The more regex we add, the more likely we will pick up things that aren't phone numbers
-
-For the outputs we have the phone number, the function that found it and the url it was found at
 """
 
 import re
@@ -11,8 +9,13 @@ import bs4
 
 
 def find_by_data_field(soup: bs4.BeautifulSoup, url):
-    # Finding a div with data-field of phone, get just the numbers
-    # Then find numbers with the format 07{+9 other digits}, +447{+9 other digits} or 07{+3 digits} {+5 digits}
+    """
+     Finding a div with data-field of phone, get just the numbers
+    Then find numbers with the format 07{+9 other digits}, +447{+9 other digits} or 07{+3 digits} {+5 digits}
+    :param url:
+    :param soup:
+    :return:
+    """
     phone_data_field = soup.find("div", {"data-field": "phone"})
     just_number = re.sub("\D", "", phone_data_field.text)
     found_number = re.findall(r"(07\d{9}|447\d{9}|07\d{3}[ \t]\d{6})$", just_number)[0]
@@ -20,36 +23,55 @@ def find_by_data_field(soup: bs4.BeautifulSoup, url):
 
 
 def find_from_call_to(soup: bs4.BeautifulSoup, url):
-    # find numbers that are contained in a hyperlink of 'callto' to auto call them
+    """
+    Find numbers that are contained in a hyperlink of 'callto' to auto call them
+    :param url:
+    :param soup:
+    :return:
+    """
     for a in soup.find_all("a", href=True):
         if "callto:" in a["href"]:
             clean_string = a["href"].replace("callto", "")
             return clean_string, inspect.stack()[0][3], url
-    # found_phone_number = soup.select("a[href*=callto]")[0].text # old version
-    # return found_phone_number, 1, inspect.stack()[0][3], url
     raise Exception
 
 
 def find_by_e164_format(soup: bs4.BeautifulSoup):
-    # find numbers with the standard e164 format
+    """
+    find numbers with the standard e164 format
+    https://www.twilio.com/docs/glossary/what-e164
+    :param url:
+    :param soup:
+    :return:
+    """
     found_phone_number = re.findall("^\+[1-9]\d{1,14}$", soup.text)[0].text
     return found_phone_number, inspect.stack()[0][3]
 
 
 def find_by_tel_href(soup: bs4.BeautifulSoup, url):
-    # find numbers that are contained in a hyperlink or 'tel:' to auto call them
+    """
+    Find numbers that are contained in a hyperlink or 'tel:' to auto call them
+    Exception because it's expected to break
+    This allows encapsulating try statement to catch it then move on
+    :param url:
+    :param soup:
+    :return:
+    """
     for a in soup.find_all("a", href=True):
         if "tel:" in a["href"]:
             clean_string = a["href"].replace("tel:", "")
             return clean_string, inspect.stack()[0][3], url
-    # Exception because it's expected to break
-    # This allows encapsulating try statement to catch it then move on
     raise Exception
 
 
 def find_by_standard_uk_mobile(soup: bs4.BeautifulSoup, url):
-    # Find mobile numbers with the format 07{9 other digits}, +447{9 other digits}, 07{3 digits} {5 digits}
-    # or 0{+3 digits} {3 digits} {4 digits}
+    """
+    Find mobile numbers with the format 07{9 other digits}, +447{9 other digits}, 07{3 digits} {5 digits}
+    or 0{+3 digits} {3 digits} {4 digits}
+    :param url:
+    :param soup:
+    :return:
+    """
     found_phone_number = re.findall(
         r"\s+(07\d{9}|\+447\d{9}|07\d{3}[ \t]\d{6})\s|"
         r"(07\d{9}|447\d{9}|07\d{3}[ \t]\d{6}|0\d{3}[ \t]\d{3}[ \t]\d{4})",
@@ -62,12 +84,17 @@ def find_by_standard_uk_mobile(soup: bs4.BeautifulSoup, url):
 
 
 def find_by_uk_landline(soup: bs4.BeautifulSoup, url):
-    # Find landline numbers with the format 0{+9-10 digits} or 0{+4 digits} {+6 digits}, 0{4 digits} {3 digits} {3
-    # digits} or (0){4 digits} {3 digits} {3 digits}
-    # Landlines can't start with 00 and must comply with area codes
-    # https://en.wikipedia.org/wiki/Telephone_numbers_in_the_United_Kingdom#Format
+    """
+    Find landline numbers with the format 0{+9-10 digits} or 0{+4 digits} {+6 digits}, 0{4 digits} {3 digits} {3
+    digits} or (0){4 digits} {3 digits} {3 digits}
+    Landlines can't start with 00 and must comply with area codes
+    https://en.wikipedia.org/wiki/Telephone_numbers_in_the_United_Kingdom#Format
+    :param url:
+    :param soup:
+    :return:
+    """
     found_phone_number = re.findall(
-        r"(0[1-9]\d{7,9}|0+[1-9]\d{3}[ \t]\d{5,6}|0[1-9]\d{3}[ \t]\d{3}[ \t]\d{3}|\((0)\)\[1-9]d{3}[ \t]\d{3}[ \t]\d{3})",
+        r"(0[1-9]\d{7,9}|0+[1-9]\d{3}[ \t]\d{5,6}|0[1-9]\d{3}[ \t]\d{3}[ \t]\d{3}|\((0)\)[1-9]\d{3}[ \t]\d{3}[ \t]\d{3})",
         soup.text,
     )
 
