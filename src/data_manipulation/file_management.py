30b42e3958d7ec6056f492daaf0e2e8bd679e74d
James Rocker <james.rocker@simplybusiness.co.uk>
2022-05-31 11:12:34 +0100

    add data manipulation module

---
+++ src/data_manipulation/file_management.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def data_dict_to_pandas(generator_json):
+    obj = []
+    for each in generator_json:
+        df = pd.DataFrame.from_dict(each.items(), orient='columns')
+        df = df.rename(columns={0: 'data', 1: 'data_value'})
+        df = df.set_index("data").T
+        obj.append(df)
+    return pd.concat(obj, ignore_index=True)
+
+
+def read_excel_file(excel_file):
+    df = pd.read_excel(excel_file)
+    return df['url']
+
+
+def output_excel_file(data_frame: pd.DataFrame):
+    data_frame.to_csv('output.csv')
+    return None
