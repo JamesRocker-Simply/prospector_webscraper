9842d384b601f4b6b8a14d776556397e14e809e6
James Rocker <james.rocker@simplybusiness.co.uk>
2022-05-31 11:14:55 +0100

    improved logging for missing data objects, changed output payload structure, fixed the mass web_scraper

--- src/webscraper.py
+++ src/webscraper.py
@@ -1,8 +1,9 @@
-import pandas as pd
-import requests
 import bs4
-from matcher import phone_number_finder as pnf
+import requests
+
+from data_manipulation import file_management as fm
 from matcher import email_finder as ef
+from matcher import phone_number_finder as pnf
 
 
 def _parse_function_loop(parsed_soup, list_of_functions, url):
@@ -51,12 +52,16 @@ def get_phone(base_url):
                 pass
 
     if number_found is None:
-        print("Number not found")
+        print(f"Number not found for {base_url}")
         return "", "missing", base_url
 
 
 def get_email(base_url):
-    email_match_func = [ef.find_by_regex, ef.find_by_mail_to, ef.find_by_mail_href]
+    email_match_func = [
+        ef.find_by_email_regex,
+        ef.find_by_mail_to,
+        ef.find_by_mail_href,
+    ]
     email_found = None
     for url in _url_list(base_url):
         if email_found is None:
@@ -69,7 +74,7 @@ def get_email(base_url):
                 pass
 
     if email_found is None:
-        print("Email not found")
+        print(f"Email not found for {base_url}")
         return "", "missing", base_url
 
 
@@ -78,32 +83,60 @@ def mass_webscrape(list_of_sites):
         try:
             phone = get_phone(base_url)
             email = get_email(base_url)
-            print(f"Base Url: {base_url}\nPhone: {phone}\nEmail: {email}\n")
-        #     probably needs to be a generator object to keep looping
+            output = {
+                "contact_number": phone[0],
+                "number_found_by_func": phone[1],
+                "number_found_at_url": phone[2],
+                "contact_email": email[0],
+                "email_found_by_func": email[1],
+                "email_found_at_url": email[2],
+            }
+            yield output
+
         except requests.exceptions.RequestException:
             print(
                 f"Url {base_url} either does not exist, no longer exists or does not respond"
             )
-            # continue
+            continue
         except Exception as e:
             print(e)
             print(
                 f"Unsuccessful call to {base_url} response is: {requests.get(base_url)}"
             )
-            # continue
+            continue
 
 
 def single_site_scrape(base_url):
     try:
         phone = get_phone(base_url)
         email = get_email(base_url)
-        print(f"Base Url: {base_url}\nPhone: {phone}\nEmail: {email}\n")
-        return base_url, phone, email
+        output = {
+            "contact_number": phone[0],
+            "number_found_by_func": phone[1],
+            "number_found_at_url": phone[2],
+            "contact_email": email[0],
+            "email_found_by_func": email[1],
+            "email_found_at_url": email[2],
+        }
+        return output
     except requests.exceptions.RequestException:
         print(f"{base_url} either does not exist, no longer exists or does not respond")
     except Exception as e:
         print(e)
         print(f"Unsuccessful call to {base_url} response is: {requests.get(base_url)}")
+
+
+def dry_run(site):
+    for each in site:
+        output = {
+            "contact_number": "07506748262",
+            "number_found_by_func": "dry_run",
+            "number_found_at_url": "dummy.com",
+            "contact_email": "dummy_email@gmail.com",
+            "email_found_by_func": "dry_run",
+            "email_found_at_url": "dummy.com",
+        }
+        yield output
 
 
 """
@@ -136,8 +169,16 @@ https://astaradvisory.co.uk/
 https://www.bennettsbuilding.co.uk/
 http://pasnell.co.uk/
 https://brownsofraskelf.co.uk/
-https://www.nobleaccountants.com/ # updated regex for specific area codes 
+https://www.nobleaccountants.com/
+https://www.simplybusiness.co.uk/ # True negative on the email
+http://www.drainspecialist-mennieservices.co.uk/
+
 """
+
 if __name__ == "__main__":
     # forward slash must be at the end of the site address.
-    single_site_scrape("https://www.nobleaccountants.com/")
+    # single_site_scrape("https://www.landal.co.uk/")
+    file = "list_of_sites.xlsx"
+    site_list = fm.read_excel_file(file)
+    # print(site_list)
+    fm.output_excel_file(fm.data_dict_to_pandas(dry_run(site_list)))
