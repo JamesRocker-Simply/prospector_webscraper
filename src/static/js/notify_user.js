25fe0ff78f71d08be887d062baf681546dd574b9
James Rocker <james.rocker@simplybusiness.co.uk>
2022-06-09 09:54:49 +0100

    add loading gif so the user has some form of feedback

--- src/static/js/notify_user.js
+++ src/static/js/notify_user.js
@@ -4,3 +4,13 @@ function event_log_to_notification_area(base_url) {
     node.appendChild(text_node);
     document.getElementById("notification_area").appendChild(node);
 }
+
+
+function add_loading_gif() {
+    const node = document.createElement("LI");
+    const img = document.createElement("img");
+    img.src = "https://c.tenor.com/wpSo-8CrXqUAAAAi/loading-loading-forever.gif";
+    node.appendChild(img)
+
+    document.getElementById("loading").appendChild(node);
+}
